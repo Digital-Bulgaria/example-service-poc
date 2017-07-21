14de777ad8b90a2f187973bb2647f896e820620d
Georgi Terziev <georgi_terziev@epam.com>
2017-07-21 16:34:18 +0300

    TASK (STORY) some small refactoring

--- src/main/java/com/digital/fullfilment/integration/dim/connector/JMSConfig.java
+++ src/main/java/com/digital/fullfilment/integration/dim/connector/JMSConfig.java
@@ -11,9 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 import org.springframework.jms.support.destination.BeanFactoryDestinationResolver;
 
 /**
@@ -46,14 +43,15 @@ public class JMSConfig implements BeanFactoryAware {
 		return jmsTemplate;
 	}
 
-//	// Serialize message content to json using TextMessage
-//	@Bean
-//	public MessageConverter jacksonJmsMessageConverter() {
-//		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//		converter.setTargetType(MessageType.TEXT);
-//		converter.setTypeIdPropertyName("_type");
-//		return converter;
-//	}
+	// // Serialize message content to json using TextMessage
+	// @Bean
+	// public MessageConverter jacksonJmsMessageConverter() {
+	// MappingJackson2MessageConverter converter = new
+	// MappingJackson2MessageConverter();
+	// converter.setTargetType(MessageType.TEXT);
+	// converter.setTypeIdPropertyName("_type");
+	// return converter;
+	// }
 
 }
 
--- src/test/java/com/digital/fullfilment/integration/dim/WMSReceiveTest.java
+++ src/test/java/com/digital/fullfilment/integration/dim/WMSReceiveTest.java
@@ -1,3 +1,5 @@
+package com.digital.fullfilment.integration.dim;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,23 +8,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.util.ReflectionTestUtils;
 
-import com.digital.fullfilment.integration.dim.Application;
 import com.digital.fullfilment.integration.dim.wms.WMSService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class, loader = SpringBootContextLoader.class)
 @WebAppConfiguration
-@ActiveProfiles(profiles="test")
+@ActiveProfiles(profiles = "test")
 public class WMSReceiveTest {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired(required = false)
 	private JmsTemplate jmsTemplate;
 
@@ -41,8 +41,8 @@ public class WMSReceiveTest {
 		// send message
 		logger.info("sending a test message");
 		jmsTemplate.convertAndSend("jmsInQueue", "test message");
-		
+
 		// verify
-		//verify(wmsService on message called
+		// verify(wmsService on message called
 	}
 }
